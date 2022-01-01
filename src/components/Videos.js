1a53c8692130be520e99cfe33a123680c6119d5e
antanim <abdurnouutanim11@gmail.com>
2022-01-02 01:08:03 +0600

    Fetch vidoes form firebase realtime database

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,13 +1,33 @@
-import React from "react";
-import classes from "../styles/Videos.module.css";
+import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Link } from "react-router-dom";
+import useVideos from "../hooks/useVideos";
 import Video from "./Video";
 
 const Videos = () => {
+  const [page, setPage] = useState(1);
+
+  const { loading, error, videos, hasMore } = useVideos(page);
+
   return (
-    <div className={classes.videos}>
-      <Video />
-      <Video />
-      <Video />
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 6)}
+        >
+          {videos.map(({ title, youtubeID, noq }) => (
+            <Link to="/quiz" key={youtubeID}>
+              <Video title={title} id={youtubeID} noq={noq} />
+            </Link>
+          ))}
+        </InfiniteScroll>
+      )}
+
+      {!loading && videos.length === 0 && <div>No data found!</div>}
+      {error && <div>There was a problem!</div>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 };
